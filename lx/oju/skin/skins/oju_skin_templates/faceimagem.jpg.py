1e36899aa4ebba1f17f58941605c4950cfd6f0fc
Lucas Aquino <luxcas@gmail.com>
2013-08-05 18:41:19 -0300

    oju tema

---
+++ lx/oju/skin/skins/oju_skin_templates/faceimagem.jpg.py
@@ -0,0 +1,10 @@
+## Script (Python) "faceimagem.jpg"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+return context.getImage().data
